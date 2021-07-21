abe94771d8da070b626dd11920ba20a9c0d13e76
theol <theolinardic@gmail.com>
2021-07-21 15:38:26 -0400

    Commit 7/21

---
+++ src/main/java/ucf/assignments/InventoryManager.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 5 Solution
+ *  Copyright 2021 Theodore Linardic
+ */
+
+package ucf.assignments;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class InventoryManager extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/test/java/ucf/assignments/InventoryManagerTest.java
@@ -0,0 +1,7 @@
+package ucf.assignments;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryManagerTest {
+
+}
